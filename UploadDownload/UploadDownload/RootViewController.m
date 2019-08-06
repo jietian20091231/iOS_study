eeb9b226c4349745ae678478b8d947cd766ae00a
jietian20091231 <jietian20091231@gmail.com>
2019-08-07 00:04:38 +0800

    add Activity Indicator

--- UploadDownload/UploadDownload/RootViewController.m
+++ UploadDownload/UploadDownload/RootViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "RootViewController.h"
+#import "TORoundedButton.h"
 
 @interface RootViewController ()
+@property (strong, nonatomic) UIActivityIndicatorView *activeIndicator;
 
 @end
 
@@ -17,6 +19,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    CGRect screen = [[UIScreen mainScreen] bounds];
+    _activeIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake( (screen.size.width - 40) / 2 , 86, 40, 40)];
+    _activeIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+    _activeIndicator.transform = CGAffineTransformMakeScale(2, 2);
+    
+    _activeIndicator.hidesWhenStopped = YES;
+    [self.view addSubview: _activeIndicator];
+    
+    TORoundedButton *btnUpload = [[TORoundedButton alloc] initWithText: @"Upload"];
+    [btnUpload setFrame: CGRectMake((screen.size.width - btnUpload.frame.size.width) / 2, 86 + 40 + btnUpload.frame.size.height, btnUpload.frame.size.width, btnUpload.frame.size.height)];
+    [btnUpload addTarget: self action: @selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview: btnUpload];
+    
+}
+
+- (void) btnClick: (id) sender {
+    NSLog( @"RootViewController:btnClick" );
+    if ([_activeIndicator isAnimating]) {
+        [_activeIndicator stopAnimating];
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+    } else {
+        [_activeIndicator startAnimating];
+        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    }
 }
 
 /*
