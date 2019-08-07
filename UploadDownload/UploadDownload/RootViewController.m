a30297d0ddbcd524cc57f53a2e8a06b9fe3bd661
jietian20091231 <jietian20091231@gmail.com>
2019-08-07 10:41:00 +0800

    Add NSTime, UIProgressView

--- UploadDownload/UploadDownload/RootViewController.m
+++ UploadDownload/UploadDownload/RootViewController.m
@@ -11,10 +11,18 @@
 
 @interface RootViewController ()
 @property (strong, nonatomic) UIActivityIndicatorView *activeIndicator;
+@property (nonatomic, strong) UIProgressView * progressView;
+@property (nonatomic, strong) NSTimer * timer;
+@property int downloadFlag;
 
 @end
 
 @implementation RootViewController
+
+
+- (void)viewDidAppear:(BOOL)animated {
+    _downloadFlag = 1;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -29,19 +37,64 @@
     
     TORoundedButton *btnUpload = [[TORoundedButton alloc] initWithText: @"Upload"];
     [btnUpload setFrame: CGRectMake((screen.size.width - btnUpload.frame.size.width) / 2, 86 + 40 + btnUpload.frame.size.height, btnUpload.frame.size.width, btnUpload.frame.size.height)];
-    [btnUpload addTarget: self action: @selector(btnClick:) forControlEvents:UIControlEventTouchUpInside];
+    [btnUpload addTarget: self action: @selector(uploadClick:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview: btnUpload];
     
+    CGFloat prgWidth = 300;
+    CGFloat prgHeight = 2;
+    CGFloat prgy = 283;
+    CGFloat prgx = (screen.size.width - prgWidth) / 2;
+    
+    _progressView = [[UIProgressView alloc] initWithFrame: CGRectMake(prgx, prgy, prgWidth, prgHeight)];
+    [_progressView setProgress: 0.25];
+    [self.view addSubview: _progressView];
+    
+    TORoundedButton *btnDownload = [[TORoundedButton alloc] initWithText: @"Download"];
+    [btnDownload setFrame: CGRectMake((screen.size.width - btnDownload.frame.size.width) / 2, prgy + 40, btnDownload.frame.size.width, btnDownload.frame.size.height)];
+    [btnDownload addTarget: self action: @selector(downloadClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview: btnDownload];
+
 }
 
-- (void) btnClick: (id) sender {
-    NSLog( @"RootViewController:btnClick" );
+- (void) uploadClick: (id) sender {
+    NSLog( @"RootViewController:uploadClick" );
     if ([_activeIndicator isAnimating]) {
         [_activeIndicator stopAnimating];
         [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
     } else {
         [_activeIndicator startAnimating];
         [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    }
+}
+
+- (void) download {
+    _progressView.progress += 0.1;
+    if ( _progressView.progress == 1.0 ) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Download Completed" message:@"" preferredStyle:UIAlertControllerStyleAlert];
+        
+        UIAlertAction *okAction = [UIAlertAction actionWithTitle: @"OK" style:UIAlertActionStyleDefault handler:nil];
+        
+        [alert addAction:okAction];
+        [self presentViewController: alert animated: YES completion:^{
+            self.downloadFlag = 1;
+            self.progressView.progress = 0;
+        }];
+        [_timer invalidate];
+        _timer = nil;
+        
+    }
+    
+}
+
+- (void) downloadClick: (id) sender {
+    NSLog( @"RootViewController:downloadClick" );
+    if ( 0 == _downloadFlag ) {
+        [_timer invalidate];
+        _timer = nil;
+        _downloadFlag = 1;
+    } else {
+        _timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target: self selector: @selector(download) userInfo:nil repeats:YES];
+        _downloadFlag = 0;
     }
 }
 
