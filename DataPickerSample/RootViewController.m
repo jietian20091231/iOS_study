34f1ba62688ac0625b6e37c28ef725ff30d874d2
jietian20091231 <jietian20091231@gmail.com>
2019-08-08 15:44:43 +0800

    add TORoundbutton, UIDataPicker, ChangedEvent  .etc

--- DataPickerSample/RootViewController.m
+++ DataPickerSample/RootViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "RootViewController.h"
+#import "TORoundedButton.h"
 
 @interface RootViewController ()
+@property (nonatomic, strong) UIDatePicker * dataPicker;
+@property (nonatomic, strong) UILabel * label;
 
 @end
 
@@ -17,6 +20,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    CGRect screen = [[UIScreen mainScreen] bounds];
+    CGFloat pWidth = 320;
+    CGFloat pHeight = 167;
+    
+    _dataPicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 100, pWidth, pHeight)];
+    //_dataPicker.locale = [NSLocale localeWithLocaleIdentifier:@"zh-Hans"];
+    _dataPicker.datePickerMode = UIDatePickerModeDateAndTime;
+    [_dataPicker addTarget:self action:@selector(pickerChagned:) forControlEvents:UIControlEventValueChanged];
+    
+    [self.view addSubview: _dataPicker];
+    
+    CGFloat labelH = 40;
+    CGFloat labelX = 50;
+    CGFloat labelY = 40;
+    
+    _label = [[UILabel alloc] initWithFrame: CGRectMake(labelX, _dataPicker.frame.origin.y + pHeight + labelY, screen.size.width - labelY * 2 , labelH)];
+    _label.textAlignment = NSTextAlignmentCenter;
+    _label.text = @"Label" ;
+    _label.textColor = [UIColor blueColor];
+    _label.font = [UIFont boldSystemFontOfSize:20];
+    _label.font = [UIFont fontWithName:@ "Arial Rounded MT Bold"  size:(30)];
+    
+    [self.view addSubview: _label];
+    
+    TORoundedButton *btnGetData = [[TORoundedButton alloc] initWithText: @"Reset"];
+    [btnGetData setFrame: CGRectMake((screen.size.width - btnGetData.frame.size.width) / 2, _label.frame.origin.y + 40 + btnGetData.frame.size.height, btnGetData.frame.size.width, btnGetData.frame.size.height)];
+    [btnGetData addTarget: self action: @selector(getDataClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview: btnGetData];
+    
+    
+}
+
+- (void) pickerChagned: (id) sender {
+    NSLog(@"RootViewController:pickerChagned");
+    NSDate* theDate = _dataPicker.date;
+    NSLog(@"the data picked: %@", [theDate descriptionWithLocale:[NSLocale currentLocale]]);
+    NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat= @"MM/dd/YYYY HH:mm:ss";
+    NSLog(@"the data formate is : %@", [dateFormatter stringFromDate: theDate] );
+    _label.text = [dateFormatter stringFromDate: theDate];
+}
+
+- (void) getDataClick : (id) sender {
+    NSLog(@"RootViewController:getDataClick");
+    NSDate* theDate = [NSDate date];
+    NSLog(@"the data picked: %@", [theDate descriptionWithLocale:[NSLocale currentLocale]]);
+    [_dataPicker setDate:theDate animated:YES];
+    
+    NSDateFormatter* dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat= @"MM/dd/YYYY HH:mm:ss";
+    NSLog(@"the data formate is : %@", [dateFormatter stringFromDate: theDate] );
+    _label.text = [dateFormatter stringFromDate: theDate];
+    
+    
 }
 
 /*
